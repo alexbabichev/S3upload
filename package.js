ea19afafc1193f0eecd76de5e28a5b30252900e2
Alex Ross <alex.ross.ua@gmail.com>
2014-02-19 19:06:49 +0200

    Created Package

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+	summary: "Upload files to S3. Allows use of Knox Server-Side."
+});
+
+Npm.depends({
+	knox: "0.8.5"
+});
+
+
+Package.on_use(function (api) {
+	api.use(["underscore", "ejson","service-configuration"], ["client", "server"]);
+	api.add_files('s3upload.js', 'server');
+	
+	api.export && api.export("Knox","server");
+	
+});
