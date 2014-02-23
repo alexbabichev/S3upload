1ac2df870a23e45425e9b2f8deed7650d4e02fbf
Alex Ross <alex.ross.ua@gmail.com>
2014-02-23 23:43:59 +0200

    upload with progress, upload file by path, list s2 file, etc

--- s3upload.js
+++ s3upload.js
@@ -1,46 +1,83 @@
 
-Knox = Npm.require("knox");
+var Knox = Npm.require("knox");
 var Future = Npm.require('fibers/future');
+var http = Npm.require('http');
+var fs = Npm.require('fs');
 
 var knox;
 var S3;
 
 Meteor.methods({
-	S3config:function(obj){
+	S3config: function(obj){
 		knox = Knox.createClient(obj);
 		S3 = {directory:obj.directory || "/"};
 	},
-	S3upload:function(file, context, callback){
-		var future = new Future();
+	
+	S3upload: function(fileInfo, fileData, metadata){
+		
+		var file_id = Files.insert({name: fileInfo.name, metadata: metadata});
+		
+		var folder = ''; if (metadata.folder) folder = metadata.folder + '/',
+				extension = (fileInfo.name).match(/\.[0-9a-z]{1,5}$/i),
+				S3path = S3.directory+folder+file_id+extension;
 
-		var extension = (file.name).match(/\.[0-9a-z]{1,5}$/i);
-		file.name = Meteor.uuid()+extension;
-		var path = S3.directory+file.name;
-
-		var buffer = new Buffer(file.data);
-
-		knox.putBuffer(buffer,path,{"Content-Type":file.type,"Content-Length":buffer.length},function(e,r){
-			if(!e){
-				future.return(path);
-			} else {
-				console.log(e);
-			}
+		var dir = '../../../../../tmp/',
+				temp_name = dir + file_id;
+					
+		fs.writeFile(temp_name, fileData, {encoding: 'binary'}, function(){
+			var put = knox.putFile(temp_name, S3path, function(err, res){
+				res.resume();
+			});
+			put.on('progress', function(res){
+				Files.update({_id: file_id}, {$set: {res: res}});
+			});
+			put.on('error', function(error){
+				throw new Meteor.Error(500, 'Internal Server Error', error);
+			});			
 		});
+		
+		return file_id;
+	},
+	
+	S3upload_from_path: function(){
+		
+		var file_id = Files.insert({name: filename, metadata: metadata});
 
-		if(future.wait() && callback){
-			var url = knox.http(future.wait());
-			Meteor.call(callback,url,context);
-			return url;
+		var extension = (filename).match(/\.[0-9a-z]{1,5}$/i);
+		var S3path = S3.directory+file_id+extension;		
+
+		var put = knox.putFile(file_path, S3path, function(err, res){
+			res.resume();
+		});
+		put.on('progress', function(res){
+			Files.update({_id: file_id}, {$set: {res: res}});
+		});
+		put.on('error', function(error){
+			throw new Meteor.Error(500, 'Internal Server Error', error);
+		});
+	},
+	
+	S3delete: function(path, callback){
+		knox.deleteFile(path, function(error, data) {
+			if (!error)
+				future.return(data);
+			else throw new Meteor.Error(500, 'Internal Server Error');
+		});
+		if(future.wait()){
+			return future.wait();
 		}
 	},
-	S3delete:function(path, callback){
-		knox.deleteFile(path, function(e,r) {
-			if(e){
-				console.log(e);
-			}	else if(callback){
-				Meteor.call(callback);
-			}
+	
+	S3list: function(){
+		var future = new Future();
+		knox.list({ prefix: 'my-prefix' }, function(error, data){
+			if (!error)
+				future.return(data);
+			else throw new Meteor.Error(500, 'Internal Server Error');
 		});
+		if(future.wait()){
+			return future.wait();
+		}
 	}
 	
 });
